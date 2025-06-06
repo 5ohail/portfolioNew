6d6213a3b85945c33f988e02ecc69752f6e5a5af
5ohail <sohailansarisa318@gmail.com>
2025-06-07 00:39:56 +0530

    removed unnecessary routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
-import SmoothScroll from './components/SmoothScroll'
 
 
 const App = () => {
   return (
-   <SmoothScroll>
      <Home/>
-   </SmoothScroll>
   )
 }
 
